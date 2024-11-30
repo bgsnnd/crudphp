1c8486ef0b47405ab74eaad790e29116ebbe4a37
bgsnnd <bgsnnd8@gmail.com>
2024-11-30 11:50:21 +0700

    y

--- crud.sql
+++ crud.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Nov 2024 pada 17.53
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.0.30
+-- Generation Time: Nov 30, 2024 at 05:49 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pegawai`
+-- Table structure for table `pegawai`
 --
 
 CREATE TABLE `pegawai` (
@@ -34,36 +34,38 @@ CREATE TABLE `pegawai` (
   `jabatan` varchar(20) NOT NULL,
   `no_hp` varchar(15) NOT NULL,
   `alamat` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `pegawai`
+-- Dumping data for table `pegawai`
 --
 
 INSERT INTO `pegawai` (`id`, `nik`, `nama_lengkap`, `jabatan`, `no_hp`, `alamat`) VALUES
-(14, '2025011999', 'Bagus Nanda', 'prakomap', '085746418754', 'Jl. Kendalsari III No. 86'),
-(15, '2025012000', 'Tom Haye', 'prakomam', '085746897254', 'Jakarta');
+(14, '2025011999', 'Justin Hubner', 'prakomap', '085746418754', 'Kediri'),
+(15, '2025012000', 'Tom Haye', 'prakomam', '085746897254', 'Jakarta'),
+(16, '2025012000951', 'Jay Idzes', 'prakomam', '0854637216145', 'Malang'),
+(17, '2025012000952', 'Ivar Jenner', 'prakomap', '0854637216145', 'Jember');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `pegawai`
+-- Indexes for table `pegawai`
 --
 ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nik` (`nik`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `pegawai`
+-- AUTO_INCREMENT for table `pegawai`
 --
 ALTER TABLE `pegawai`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
